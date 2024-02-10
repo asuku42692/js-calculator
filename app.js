45946fd2d000b0de1c6b5710484eb7bb756cff1f
asuku42692 <asukuhamza1@gmail.com>
2024-02-10 15:56:55 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+function print(value) {
+
+    let input = document.getElementById("input");
+    
+    input.value += value;
+
+    if (input.value.includes("c")) {
+        input.value = "";
+    }
+
+}
+
+
+function compute() {
+    let v = document.getElementById("input").value;
+    let result = eval(v);
+    document.getElementById("input").value = result;
+}
